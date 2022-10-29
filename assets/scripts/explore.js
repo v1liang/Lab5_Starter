dcfe83788fb76d983fdaf52d9a3bef3593e4c5d8
Vincent <v1liang@ucsd.edu>
2022-10-29 09:19:43 -0700

    Finish explore.js and expose.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,49 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // Populate voices
+  let voices = [];
+  const synth = window.speechSynthesis;
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i ++){
+      const option = document.createElement('option');
+      option.textContent =  `${voices[i].name} (${voices[i].lang})`;
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      document.getElementById("voice-select").appendChild(option);
+    }
+  }
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  const button = document.querySelector("button");
+  const inputTxt = document.querySelector("textarea");
+  const select = document.getElementById("voice-select");
+
+  button.addEventListener('click', function() {
+    const index = select.selectedIndex;
+    const speak = new SpeechSynthesisUtterance(inputTxt.value);
+    const selectedOption = select[index].getAttribute('data-name');
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name == selectedOption) {
+        speak.voice = voices[i];
+      }
+    }
+    const synth = window.speechSynthesis;
+    synth.speak(speak);
+    const img = document.querySelector("img"); 
+    img.src = "assets/images/smiling-open.png"
+    const speakCheck = setInterval(function(){
+      if(synth.speaking == false){
+        img.src = "assets/images/smiling.png";
+        clearInterval(speakCheck);
+      }
+    }, 1)
+  });
 }
+
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,65 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  //CORRECT IMAGE + AUDIO SECTION
+  const hornDOM = document.getElementById('horn-select');
+  const imgDOM = document.querySelector('img');
+  const audioDOM = document.querySelector('audio');
+
+  hornDOM.addEventListener('change', function(){
+    console.log(hornDOM.value);
+    if(hornDOM.value == 'air-horn') {
+      imgDOM.src="assets/images/air-horn.svg";
+      audioDOM.src="assets/audio/air-horn.mp3";
+      console.log(audioDOM.src);
+    }
+    if(hornDOM.value == 'car-horn') {
+      imgDOM.src="assets/images/car-horn.svg";
+      audioDOM.src="assets/audio/car-horn.mp3";
+    }
+    if(hornDOM.value == 'party-horn') {
+      imgDOM.src="assets/images/party-horn.svg";
+      audioDOM.src="assets/audio/party-horn.mp3";
+    }
+  });
+  //CORRECT VOLUME SECTION
+  const volumeDOM = document.getElementById('volume-controls');
+  const volumeInput = volumeDOM.querySelector("input[value='50']");
+  const volumeImg = volumeDOM.querySelector('img');
+
+  volumeInput.addEventListener('click', function() {
+    let audioVal = volumeInput.value;
+    audioDOM.volume = audioVal*0.01;
+    if(audioVal == 0) {
+      volumeImg.src="assets/icons/volume-level-0.svg";
+      volumeImg.alt="Volume level 0";
+    }
+    if(audioVal >= 1 && audioVal < 33) {
+      volumeImg.src="assets/icons/volume-level-1.svg";
+      volumeImg.alt="Volume level 1";
+    }
+    if(audioVal >= 33 && audioVal < 67) {
+      volumeImg.src="assets/icons/volume-level-2.svg";
+      volumeImg.alt="Volume level 2";
+    }
+    if(audioVal >= 67) {
+      volumeImg.src="assets/icons/volume-level-3.svg";
+      volumeImg.alt="Volume level 3";
+    }
+    // console.log(volumeImg);
+    // console.log(audioVal.value);
+  });
+  // Play Sound
+  const buttonDOM = document.querySelector('button');
+  buttonDOM.addEventListener('click', function() {
+    audioDOM.play();
+    if(hornDOM.value == 'party-horn') {
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti({
+        confettiRadius: 50
+      });
+    }
+    console.log(audioDOM);
+  });
 }
